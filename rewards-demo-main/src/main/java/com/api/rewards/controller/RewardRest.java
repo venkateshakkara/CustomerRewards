329509c56fc4c98a2ba92d2cbac7c6bd6df014fe
venkatesh.aka@gmail.com <Jaimatadi909$>
2025-03-02 00:19:06 -0600

    Added swagger

--- rewards-demo-main/src/main/java/com/api/rewards/controller/RewardRest.java
+++ rewards-demo-main/src/main/java/com/api/rewards/controller/RewardRest.java
@@ -2,6 +2,9 @@ package com.api.rewards.controller;
 
 import com.api.rewards.constants.CommonConstants;
 import com.api.rewards.entity.Customer;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,11 @@ public interface RewardRest {
 
 
  @GetMapping ( value = CommonConstants.GET_CUSTOMER_URL)
+ @ApiOperation(value = "Get Sample Data", response = String.class)
+ @ApiResponses(value = {
+         @ApiResponse(code = 200, message = "Successfully retrieved sample data"),
+         @ApiResponse(code = 404, message = "Sample data not found")
+ })
  ResponseEntity<List<Customer>> findAllCustomers();
 
  @GetMapping ( value = CommonConstants.GET_CUSTOMERS_BY_ID_URL)
